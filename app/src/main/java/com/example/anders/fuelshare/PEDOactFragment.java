825c3c386cbceefeffd72dd41f63c7ce3f18bb27
bakhoj <anders2108@gmail.com>
2015-10-07 15:26:56 +0200

    added pedo activity and fragment

---
+++ app/src/main/java/com/example/anders/fuelshare/PEDOactFragment.java
@@ -0,0 +1,22 @@
+package com.example.anders.fuelshare;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PEDOactFragment extends Fragment {
+
+    public PEDOactFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pedoact, container, false);
+    }
+}
