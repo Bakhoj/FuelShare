ec3bae90732b32fe188765e91d37e13c66b3613a
LarsPede <larspeter94@gmail.com>
2015-10-07 17:07:07 +0200

    Algo opsætning

--- app/src/main/java/com/example/anders/fuelshare/Algo.java
+++ app/src/main/java/com/example/anders/fuelshare/Algo.java
@@ -1,9 +1,35 @@
 package com.example.anders.fuelshare;
 
 /**
- * Created by anders on 07-10-2015.
+ * @author Lars
+ *
+ * info: This Class handles all the calculations for PEDO
  */
 public class Algo {
-    private int batLvl, dist, vel, bPedel; // BatterieLevel, distance, velocity, breakPedel
 
+    private int batLvl, dist, vel, bPedal; // BatteryLevel, distance, velocity, breakPedal
+
+    //Method to check the bat level.
+    //input: Battery reading CAN Bus.
+    //output: int value from 1 - 6 describing Battery lvl.
+    public int BatLvl(){
+
+        return 0;
+    }
+
+    //Method to check how many kilometers you can drive at best.
+    //input: Battery reading and Distance CAN Bus.
+    //output: int value describing kilometers left.
+    public int DistLeft(){
+
+        return 0;
+    }
+
+    //Method to check consumation rate.
+    //input: Battery reading and Distance CAN Bus.
+    //output: int value //TODO needs to be described.
+    public int BatDist(){
+
+        return 0;
+    }
 }
