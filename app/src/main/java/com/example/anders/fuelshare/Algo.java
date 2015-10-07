4d4c3e55e2ebba062ed12227c6d070dd151ece93
bakhoj <anders2108@gmail.com>
2015-10-07 16:18:53 +0200

    Added Algo.java

---
+++ app/src/main/java/com/example/anders/fuelshare/Algo.java
@@ -0,0 +1,9 @@
+package com.example.anders.fuelshare;
+
+/**
+ * Created by anders on 07-10-2015.
+ */
+public class Algo {
+    private int batLvl, dist, vel, bPedel; // BatterieLevel, distance, velocity, breakPedel
+
+}
