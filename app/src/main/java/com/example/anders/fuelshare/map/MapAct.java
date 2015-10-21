28245883a283fac1deacb4c6d4a06cdbe899aed2
bakhoj <anders2108@gmail.com>
2015-10-21 15:53:50 +0200

    added

---
+++ app/src/main/java/com/example/anders/fuelshare/map/MapAct.java
@@ -0,0 +1,16 @@
+package com.example.anders.fuelshare.map;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.anders.fuelshare.R;
+
+public class MapAct extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_map);
+    }
+
+}
