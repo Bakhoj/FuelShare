c404f794ce9129d348ec669d3e19124251cc3878
LarsPede <larspeter94@gmail.com>
2015-10-28 15:41:04 +0100

    no errors

--- app/src/main/java/com/example/anders/fuelshare/map/MapAct.java
+++ app/src/main/java/com/example/anders/fuelshare/map/MapAct.java
@@ -3,13 +3,14 @@ package com.example.anders.fuelshare.map;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.example.anders.fuelshare.R;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import android.R;
 
 public class MapAct extends FragmentActivity implements OnMapReadyCallback {
 
